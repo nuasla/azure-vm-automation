41dc50f2a861a759523ad3b35c5ffba1cbed62de
Numan A <numanaslam0@gmail.com>
2022-05-03 09:59:08 +0200

    Bicep

---
+++ Oppgave6-bicep.ps1
@@ -0,0 +1,22 @@
+param(
+    [string[]]$rgNames
+)
+# function/commando for creating a resource group
+
+# NB!! Wich tenant/subscription
+# You can use different browsers with different colors for different tenants
+# Activate the right browswer, then do Connect-AzAccount
+
+# How to define the correct subscription, in case of access to many
+
+## ASSUMPTION - already logged in correct tenant
+
+Set-AzContext -SubscriptionId <Name>
+
+foreach ($rgName in $rgNames) {
+    New-AzDeployment -Name "Deployment1" -Location "norwayeast" -TemplateFile "./rg.bicep" -TemplateParameterObject @{rgName=@{value=$rgName}}
+    #New-AzResourceGroup -Name $rgName -Location "norwayeast"
+}
+
+# After completion - remember Disconnect-AzAccount -Scope CurrentUser
+
